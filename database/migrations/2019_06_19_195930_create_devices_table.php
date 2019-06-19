88da20b46f3c2f26d02b4e7d72beaf2aadbc0b0e
İsmail Çakır <ismailcakirnet@gmail.com>
2019-06-19 23:51:18 +0300

    added migrations

---
+++ database/migrations/2019_06_19_195930_create_devices_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Device Information
+            $table->string('device_token',250)->unique()->index('device_token'); // Unique device token
+            $table->string('access_token', 32)->nullable();
+            $table->string('language_code', 10)->nullable(); // Ex: tr_TR, en_US
+            $table->string('region_code', 10)->nullable(); // Ex: TR, US
+            $table->string('platform', 50)->nullable(); // Ex: ios, android other ex. apple watch, android gear
+            $table->string('notification_token', 250)->default(false); // Ex: Onesignal platform player token.
+            $table->string('notification_tags', 250)->default(false); // Ex: daily, weekly, monthly, premium
+
+            // App Information
+            $table->string('app_version', 50)->nullable(); // Device current app version. Ex: 1.0.1, 1.0.2
+            $table->boolean('is_premium')->default(false); // Device premium status.
+
+            // Timestamp
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
---
+++ database/migrations/2019_06_19_201705_create_songs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('thumbnail', 250)->nullable();
+            $table->string('media')->nullable(); // media source path or url
+
+            // Dates
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('song_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('song_id')->unsigned();
+            $table->string('name'); // translate locale name
+            $table->string('locale')->index();
+
+            // Relations
+            $table->unique(['song_id', 'locale']);
+            $table->foreign('song_id')->references('id')->on('songs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('song_translations');
+        Schema::dropIfExists('songs');
+    }
+}

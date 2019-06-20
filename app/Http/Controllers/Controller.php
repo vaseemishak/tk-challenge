63f0f2f3e2652bcd7b2e61425cb7f641a7000246
İsmail Çakır <ismailcakirnet@gmail.com>
2019-06-21 00:12:09 +0300

    add api response skeleton

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param $data
+     * @param int $status
+     * @param array $headers
+     * @param null $error
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function response($data, $status = 200, $error = null, $headers = [])
+    {
+        return response()->json([
+            'status' => $status,
+            'error' => $error,
+            'data'    => $data
+        ], $status, $headers);
+    }
+
 }

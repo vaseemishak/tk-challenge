d6ec519c89aba76dced71645a95b9c746e9cdf9d
İsmail Çakır <ismailcakirnet@gmail.com>
2019-06-22 18:34:52 +0300

    fixed device migration columns

--- database/migrations/2019_06_19_195930_create_devices_table.php
+++ database/migrations/2019_06_19_195930_create_devices_table.php
@@ -22,8 +22,8 @@ class CreateDevicesTable extends Migration
             $table->string('language_code', 10)->nullable(); // Ex: tr_TR, en_US
             $table->string('region_code', 10)->nullable(); // Ex: TR, US
             $table->string('platform', 50)->nullable(); // Ex: ios, android other ex. apple watch, android gear
-            $table->string('notification_token', 250)->default(false); // Ex: Onesignal platform player token.
-            $table->string('notification_tags', 250)->default(false); // Ex: daily, weekly, monthly, premium
+            $table->string('notification_token', 250)->nullable(); // Ex: Onesignal platform player token.
+            $table->string('notification_tags', 250)->nullable(); // Ex: daily, weekly, monthly, premium
 
             // App Information
             $table->string('app_version', 50)->nullable(); // Device current app version. Ex: 1.0.1, 1.0.2

0f415a627dc76539fe0a1c623341baab2e57f908
İsmail Çakır <ismailcakirnet@gmail.com>
2019-06-22 18:34:23 +0300

    updated validation response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -29,8 +30,9 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
      * @return void
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -46,6 +48,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ValidationException)
+        {
+            return response()->json([
+                'message' => $exception->getMessage(),
+                'data'   => null,
+                'status' => $exception->status,
+                'errors' => $exception->errors(),
+            ], $exception->status);
+        }
+
         return parent::render($request, $exception);
     }
 }
